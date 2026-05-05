8e630769c65fce5ed28a5f93143d9ae13974a85f
Diptendu16 <188218888+Diptendu16@users.noreply.github.com>
2026-05-05 22:22:56 +0530

    add auth middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,2 +1,24 @@
 import jwt from "jsonwebtoken";
 
+//get token from cookie
+//validate token
+//verify the token
+//send details ad req.user
+
+const authMiddleware = (req, res, next) => {
+  const token = req.cookies.token;
+
+  if (!token) {
+    return res.status(401).json({ error: "Access denied. Please log in" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded.userId;
+    next();
+  } catch (err) {
+    res.status(200).json({ error: "Invalid token" });
+  }
+};
+
+export default authMiddleware;
